d24f8ad0a625cc09d5fbccf0888a4dbe2bc1a9e0
Z-Szabadkai <zoltan.szabadkai@gmail.com>
2021-09-01 19:37:15 +0200

    Added method to find the minimum value

--- src/TeljesenMindegyMi.java
+++ src/TeljesenMindegyMi.java
@@ -4,4 +4,14 @@ public class TeljesenMindegyMi {
 
         int[] mindegyTomb = {34, 54, 634, 21, 423, 35, 73, 1, 90, 1, 55, 68, 37, 94, 61, 75, 88, 999, 843, 200};
     }
+
+    public static int findMinimum(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
 }
