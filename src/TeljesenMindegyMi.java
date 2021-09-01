43dcaf3552856c67b00e95e41c783ffd3b052064
thawid087 <thawidstock@gmail.com>
2021-09-01 19:19:55 +0200

    itt van a 20 számos tömb

---
+++ src/TeljesenMindegyMi.java
@@ -0,0 +1,7 @@
+public class TeljesenMindegyMi {
+
+    public static void main(String[] args) {
+
+        int[] mindegyTomb = {34, 54, 634, 21, 423, 35, 73, 1, 90, 1, 55, 68, 37, 94, 61, 75, 88, 999, 843, 200};
+    }
+}
