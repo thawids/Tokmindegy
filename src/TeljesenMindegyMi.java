2f32fe07ef12bd482da1658767f71d3ca9664afe
thawid087 <thawidstock@gmail.com>
2021-09-01 19:49:25 +0200

    minimum után itt a maximum

--- src/TeljesenMindegyMi.java
+++ src/TeljesenMindegyMi.java
@@ -14,4 +14,13 @@ public class TeljesenMindegyMi {
         }
         return min;
     }
+    public static int findMaximum(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < max) {
+                max = array[i];
+            }
+        }
+        return max;
 }
+    }
